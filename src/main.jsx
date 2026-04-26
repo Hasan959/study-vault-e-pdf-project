a8b0ebe6d14d41c95fefa8f6fea0eb7cdd49a225
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-04-26 23:47:12 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router";
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
